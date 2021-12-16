66ecb8e90a4b46fc28c407dd1fdb5bba307d9204
Daffa404 <58094200+Daffa404@users.noreply.github.com>
2021-12-16 23:12:34 +0700

    4

--- src/assetsLoader.js
+++ src/assetsLoader.js
@@ -5,7 +5,7 @@ var bishopModel;
 var knightModel;
 var rookModel;
 var whiteSectionTexture = new THREE.TextureLoader().load("../assets/textures/white_section.jpg");
-var blackSectionTexture = new THREE.TextureLoader().load("../assets/textures/white_section.jpg");
+var blackSectionTexture = new THREE.TextureLoader().load("../assets/textures/black_section.jpg");
 var whitePieceTexture = new THREE.TextureLoader().load("../assets/textures/white_piece.jpg");
 var blackPieceTexture = new THREE.TextureLoader().load("../assets/textures/black_piece.jpg");
 var piecesModelPath = "../assets/model/";
